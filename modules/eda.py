073b8a78a632d4eda4dbd1fa4aeefbbbeb6a68cd
Linfo-KR <linfo4931@gmail.com>
2024-05-26 00:12:59 +0900

    correlation 기능 추가 및 PDF 가시성 개선

--- modules/eda.py
+++ modules/eda.py
@@ -1,10 +1,13 @@
 import io
+import warnings
 
+import datetime as dt
 import numpy as np
 import pandas as pd
 
 from fpdf import FPDF
 
+warnings.filterwarnings('ignore')
 
 class EDA:
     """
@@ -16,16 +19,21 @@ class EDA:
         
     Methods:
         _convert_to_df(input) : pd.DataFrame / list / dict / np.ndarray를 pd.DataFrame으로 변환하는 함수.
-        info() : Input Data에 대한 Information을 출력(Row, Col 수 / Head, Tail / ColName, Data Types / Missing Values 수)하는 함수.
-        summary() : Input Data의 기술통계를 출력(Numerical Values / Categorical Values)하는 함수.
+        info() : Input Data에 대한 Information을 출력(Row, Col 수 / Head, Tail / ColName, Data Types / Missing Values 수 / Dupicated Values 수)하는 함수.
+        summary() : Input Data의 기술통계를 출력(Numerical Values / Categorical Values / Datetime Values)하는 함수.
+        correlation() : Input Data의 Numerical Features를 대상으로 변수 간 상관관계(Pearson Correlation)를 출력하는 함수.
         save_markdown(filename) : info(), summary() Function의 결과를 Markdown Documents로 저장하는 함수.
         save_pdf(filename) : info(), summary() Function의 결과를 PDF Documents로 저장하는 함수.
+        
+    Update:
+        Processing...
     """
     
     def __init__(self, input):
         self.input = self._convert_to_df(input)
         self.num_cols = self.input.select_dtypes(include=[np.number]).columns
         self.cat_cols = self.input.select_dtypes(include=['object']).columns
+        self.dt_cols = self.input.select_dtypes(include=['datetime']).columns
     
     def _convert_to_df(self, input):
         if isinstance(input, pd.DataFrame):
@@ -55,8 +63,11 @@ class EDA:
         self.input.info(buf=buffer)
         
         print('\n\n1.4. Missing Values', file=buffer)
-        print(self.input.isnull().sum(), '\n\n', file=buffer)
+        print(self.input.isnull().sum(), file=buffer)
         
+        print('\n\n1.5. Duplicated Values', file=buffer)
+        print(self.input.duplicated().sum(), file=buffer)
+                
         info_str = buffer.getvalue()
         buffer.close()
         
@@ -77,20 +88,46 @@ class EDA:
             for col in self.cat_cols:
                 sums = self.input[col].value_counts()
                 print(f'{col} : {sums}\n', file=buffer)
-            print('\n\n', file=buffer)
+            print('\n', file=buffer)
         else:
             print('2.2. Categorical Features Summary', file=buffer)
             print("Input Data's Categorical Columns aren't Exist.\n\n", file=buffer)
             pass
         
+        if len(self.dt_cols) > 0:
+            print('2.3. Datetime Features Summary', file=buffer)
+            print(self.input[self.dt_cols].agg(['min', 'max', 'nunique']), file=buffer)
+            print(self.input[self.dt_cols].dt.year.value_counts(), file=buffer)
+            print(self.input[self.dt_cols].dt.month.value_counts(), file=buffer)
+            print(self.input[self.dt_cols].dt.day.value_counts(), file=buffer)
+            print(self.input[self.dt_cols].dt.dayofweek.value_counts(), file=buffer)
+        else:
+            print('2.3. Datetime Features Summary', file=buffer)
+            print("Input Data's Datetime Columns aren't Exist.\n\n", file=buffer)
+            pass
+                
         summary_str = buffer.getvalue()
         buffer.close()
         
         return summary_str
+    
+    def correlation(self):
+        buffer = io.StringIO()
+        # if self.input[self.num_cols].isnull().any().any():
+        #     print("Input Data's Numerical Columns have Missing Values (NULL or NaN).\n\n", file=buffer)
+        #     pass
+    
+        correlation_matrix = round(self.input[self.num_cols].corr(method='pearson'), 2)
+        correlation_matrix.to_string(buf=buffer)
+        correlation_str = buffer.getvalue()
+        buffer.close()
+    
+        return correlation_str
         
     def save_markdown(self, filename):
         info_str = self.info()
         summary_str = self.summary()
+        correlation_str = self.correlation()
         
         with open(filename, 'w', encoding='utf-8') as f:
             f.write("EDA Report\n\n")
@@ -98,25 +135,35 @@ class EDA:
             f.write(info_str)
             f.write("\n\n02. Data Summary\n\n")
             f.write(summary_str)
+            f.write("\n\n03. Data Correlation Analysis\n\n")
+            f.write(correlation_str)
             
     def save_pdf(self, filename):
         info_str = self.info()
         summary_str = self.summary()
+        correlation_str = self.correlation()
         
-        document = FPDF()
+        document = FPDF(orientation='L')
         document.add_page()
-        document.set_font('Arial', 'B', size=16)
+        document.add_font('MalgunGothic', '', 'C:/Windows/Fonts/malgun.ttf', uni=True)
+        document.set_font('MalgunGothic', '', size=16)
         document.cell(w=200, h=10, txt='EDA Report\n\n', ln=1, align='L')
         
-        document.set_font('Arial', 'B', size=14)
+        document.set_font('MalgunGothic', '', size=14)
         document.cell(w=200, h=10, txt='01. Data Information\n\n', ln=1, align='L')
-        document.set_font('Arial', size=12)
+        document.set_font('MalgunGothic', '', size=10)
         document.multi_cell(w=0, h=5, txt=info_str)
         
         document.add_page()
-        document.set_font('Arial', 'B', size=14)
+        document.set_font('MalgunGothic', '', size=14)
         document.cell(w=200, h=10, txt='02. Data Summary\n\n', ln=1, align='L')
-        document.set_font('Arial', size=12)
+        document.set_font('MalgunGothic', '', size=10)
         document.multi_cell(w=0, h=5, txt=summary_str)
+        
+        document.add_page()
+        document.set_font('MalgunGothic', '', size=14)
+        document.cell(w=200, h=10, txt='03. Data Correlation Analysis\n\n', ln=1, align='L')
+        document.set_font('MalgunGothic', '', size=8)
+        document.multi_cell(w=0, h=5, txt=correlation_str)
         
         document.output(filename)
