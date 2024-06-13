1903a30e114fce818e0e68ef628424dd78897199
Linfo-KR <linfo4931@gmail.com>
2024-06-14 02:31:14 +0900

    Error Fix _save_plot() and visualize_numerical()

--- eda/visualization.py
+++ eda/visualization.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from data_loader import DataLoader
+from utils import *
 
 class Visualization:
     def __init__(self, input, output_dir, style='seaborn', font_scale=1.2, palette='deep', rc_params=None):
@@ -49,7 +50,7 @@ class Visualization:
         plt.clf()
         plt.close()
         
-    def _save_plot(self, plot_func, plot_name, *args, cols=None, subplots=True, col_length=None, subplot_name=None, **kwargs):
+    def _save_plot(self, plot_func, plot_name, *args, cols=None, subplots=True, col_length=None, box_violin=True, hue_length=None, **kwargs):
         """
         플롯을 저장하는 메서드
         
@@ -67,46 +68,74 @@ class Visualization:
             subplot의 사용 여부 (default=True)
         col_length : int
             subplot에 전달할 열의 길이
+        box_violin : boolean
+            boxplot and violinplot 사용 여부 (default=True)
         subplot_name : str
             subplot에 전달할 타이틀 이름
         **kwargs : dict
             플롯 함수에 전달할 키워드 인자
         """
         if subplots:
-            n = None
-            max_n = 20
-            for i in range(1, (max_n+1)):
-                if (i-1)**2 < col_length < i**2:
-                    n = i
-                    break
-            else:
-                raise ValueError(f'col_length({col_length}) is larger than max_n({max_n}).')
-            
-            fig, axes = plt.subplots(n, n, figsize=(20, 20))
-            axes = axes.flatten()
-            
-            for idx, col in enumerate(cols):
-                if idx >= n * n:
-                    break
-                plot_func(self.input[col], ax=axes[idx], *args, **kwargs)
-                axes[idx].set_title(subplot_name)
+            if box_violin:
+                n_h = calculate_ndim(col_length=hue_length, max_n=20)
+                fig_h, axes_h = plt.subplots(n_h, n_h, figsize=(20, 20))
+                axes_h = axes_h.flatten()
                 
-            for ax in axes[len(cols):]:
-                fig.delaxes(ax)
-            plt.tight_layout()
-            plt.savefig(os.path.join(self.output_dir, f"{plot_name}.png"))
-            
+                for idx, col in enumerate(cols):
+                    if 'x' in kwargs:
+                        hues = kwargs['x']
+                        print(f"Start hues: {hues}")
+                        if not isinstance(hues, list):
+                            hues = [hues]
+                        for idx_h, hue in enumerate(hues):
+                            if idx_h >= n_h * n_h:
+                                break
+                            kwargs['x'] = hue
+                            kwargs['y'] = col
+                            print(f"(x, y): {kwargs['x']}, {kwargs['y']}")
+                            plot_func(ax=axes_h[idx_h], *args, **kwargs)
+                            axes_h[idx_h].set_xlabel(hue)
+                            axes_h[idx_h].set_ylabel(col)
+                            
+                        kwargs['x'] = hues
+                        print(f"End hues: {kwargs['x']}")
+                        
+                        if len(axes_h) > len(hues):
+                            for ax_h in axes_h[len(hues):]:
+                                fig_h.delaxes(ax_h)
+                
+                        plt.tight_layout()
+                        plt.savefig(os.path.join(self.output_dir, f"{plot_name}_{col}.png"))
+                self._clear_plot()
+                print(f"Generating Plot : {plot_name}_{col} in {self.output_dir}")
+            else:
+                n = calculate_ndim(col_length=col_length, max_n=20)
+                fig, axes = plt.subplots(n, n, figsize=(20, 20))
+                axes = axes.flatten()
+                
+                for idx, col in enumerate(cols):
+                    if idx >= n * n:
+                        break
+                    plot_func(self.input[col], ax=axes[idx], *args, **kwargs)
+                
+                if len(axes) > len(cols):
+                    for ax in axes[len(cols):]:
+                        fig.delaxes(ax)
+                        
+                plt.tight_layout()
+                plt.savefig(os.path.join(self.output_dir, f"{plot_name}.png"))
+                self._clear_plot()
+                print(f"Generating Plot : {plot_name} in {self.output_dir}")
         else:
             plt.figure()
             plot_func(*args, **kwargs)
             plt.title(plot_name)
             plt.tight_layout()
             plt.savefig(os.path.join(self.output_dir, f"{plot_name}.png"))
-                    
-        self._clear_plot()
-        print(f"Generating Plot : {plot_name} in {self.output_dir}")
+            self._clear_plot()
+            print(f"Generating Plot : {plot_name} in {self.output_dir}")
         
-    def visualize_numerical(self, bins=30, box_violin_y=None, hue=None):
+    def visualize_numerical(self, bins=30, hue_list=list):
         """
         Numerical Data를 시각화하는 메서드
         
@@ -114,26 +143,27 @@ class Visualization:
         -----------
         bins : int, optional
             histogram의 bin 수 (default=30)
-        box_violin_y : str, optional
-            boxplot & violinplot의 y축 데이터
-        hue : str, optional
-            Group화할 열 이름
+        hue : list
+            Group화할 열의 list
+        y_hue_none : boolean
+            box_violin_y와 hue의 None 여부
         """
         # histogram / histogram_kde / kde
-        self._save_plot(sns.histplot, 'histogram', cols=self.num_cols, subplots=True, col_length=len(self.num_cols), bins=bins, kde=False)
-        self._save_plot(sns.histplot, 'histogram_kde', cols=self.num_cols, subplots=True, col_length=len(self.num_cols), bins=bins, kde=True)
-        self._save_plot(sns.kdeplot, 'kde', cols=self.num_cols, subplots=True, col_length=len(self.num_cols), fill=True)
+        self._save_plot(sns.histplot, 'histogram', cols=self.num_cols, subplots=True, col_length=len(self.num_cols), box_violin=False, bins=bins, kde=False)
+        self._save_plot(sns.histplot, 'histogram_kde', cols=self.num_cols, subplots=True, col_length=len(self.num_cols), box_violin=False, bins=bins, kde=True)
+        self._save_plot(sns.kdeplot, 'kde', cols=self.num_cols, subplots=True, col_length=len(self.num_cols), box_violin=False, fill=True)
         
         # boxplot / violinplot
-        if box_violin_y:
-            if hue is not None and hue in self.num_cols:
-                self._save_plot(sns.boxplot, f"boxplot_{box_violin_y}_{hue}", subplots=False, x=hue, y=box_violin_y, data=self.input)
-                self._save_plot(sns.violinplot, f"violinplot_{box_violin_y}_{hue}", subplots=False, x=hue, y=box_violin_y, data=self.input)
-            else:
-                self._save_plot(sns.boxplot, f"boxplot_{box_violin_y}", subplots=False, y=self.input[box_violin_y])
-                self._save_plot(sns.violinplot, f"violinplot_{box_violin_y}", subplots=False, y=self.input[box_violin_y])
+        if hue is not None and isinstance(hue, list):
+        # if hue is exist
+            self._save_plot(sns.boxplot, 'boxplot', cols=self.num_cols, subplots=True, hue_length=len(hue), box_violin=True, x=hue_list, data=self.input)
+            self._save_plot(sns.violinplot, 'violinplot', cols=self.num_cols, subplots=True, hue_length=len(hue), box_violin=True, x=hue_list, data=self.input)
         else:
-            print(f"Any box_violin_y column specified for boxplot and violinplot.")
+            print('hue_list is Empty.')
+            
+        # if hue isn't exist
+        self._save_plot(sns.boxplot, 'boxplot', cols=self.num_cols, subplots=True, col_length=len(self.num_cols), box_violin=False)
+        self._save_plot(sns.violinplot, 'violinplot', cols=self.num_cols, subplots=True, col_length=len(self.num_cols), box_violin=False)
             
         def visualize_categorical(self):
             """
@@ -148,19 +178,25 @@ class Visualization:
             hue : str, optional
                 Group화할 열 이름
             """
+            # barplot
             
-# df = sns.load_dataset('titanic')
-# rc_params = {
-#     'figure.figsize': (16, 12),
-#     'axes.titlesize': 15,
-#     'axes.labelsize': 12,
-#     'xtick.labelsize': 10,
-#     'ytick.labelsize': 10,
-#     'legend.fontsize': 12,
-#     'axes.grid': True,
-#     'grid.alpha': 0.3,
-#     'axes.unicode_minus': False,
-#     'font.family': 'Malgun Gothic'
-# }
-# viz = Visualization(df, output_dir='./test/visualize_test/', style='whitegrid', font_scale=1.2, palette='deep', rc_params=rc_params)
-# viz.visualize_numerical(bins=15, box_violin_y='fare', hue='pclass')
+            # pieplot
+            
+            # countplot
+            
+df = sns.load_dataset('titanic')
+rc_params = {
+    'figure.figsize': (16, 12),
+    'axes.titlesize': 15,
+    'axes.labelsize': 12,
+    'xtick.labelsize': 10,
+    'ytick.labelsize': 10,
+    'legend.fontsize': 12,
+    'axes.grid': True,
+    'grid.alpha': 0.3,
+    'axes.unicode_minus': False,
+    'font.family': 'Malgun Gothic'
+}
+viz = Visualization(df, output_dir='./test/visualize_test/', style='whitegrid', font_scale=1.2, palette='deep', rc_params=rc_params)
+hue = ['survived', 'pclass', 'sex', 'embarked']
+viz.visualize_numerical(bins=15, hue_list=hue)
