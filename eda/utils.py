e5e539e8e438ef7a2e09e3299a2b27e34959dd9f
Linfo-KR <linfo4931@gmail.com>
2024-06-17 10:28:39 +0900

    Change calculate_ndim()'s Logic

--- eda/utils.py
+++ eda/utils.py
@@ -1,6 +1,6 @@
+import numpy as np
 
-
-def calculate_ndim(col_length=int, max_n=20):
+def calculate_ndim(col_length=int):
     """
     col_length에 맞는 subplot의 차원을 계산하는 함수
 
@@ -8,19 +8,15 @@ def calculate_ndim(col_length=int, max_n=20):
     -----------
     col_length : int
         subplot에 전달할 열의 길이
-    max_n : int
-        최대 subplot 수 (default=20)
         
     Returns:
     --------
-    int
-        subplot의 차원 수
+    n : int
+        subplot의 차원
     """
     if not isinstance(col_length, int):
         raise ValueError(f"col_length or hue_length should be an int, got {type(col_length)} instead.")
     
-    for i in range(1, (max_n+1)):
-        if (i-1)**2 < col_length <= i**2:
-            return i
-    else:
-        raise ValueError(f"col_length({col_length}) is larger than max_n({max_n}).")
+    n = int(np.ceil(np.sqrt(col_length)))
+    
+    return n
