c10542b2adee326daa739e3f0992e1d68fd8b7f4
Linfo-KR <linfo4931@gmail.com>
2024-06-17 22:08:25 +0900

    Add on generate_testset()

--- eda/utils.py
+++ eda/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def calculate_ndim(col_length=int):
     """
@@ -20,3 +21,33 @@ def calculate_ndim(col_length=int):
     n = int(np.ceil(np.sqrt(col_length)))
     
     return n
+
+def generate_testset():
+    np.random.seed(42)
+    date_range = pd.date_range(start='2021-01-01', end='2023-12-31', freq='D')
+    df = pd.DataFrame(date_range, columns=['date'])
+    
+    df['value'] = np.random.randn(len(date_range)) * 10 + np.linspace(10, 20, len(date_range))
+    outliers = np.random.choice(len(date_range), size=5, replace=False)
+    df.loc[outliers, 'value'] *= 3
+    
+    df['temperature'] = np.random.normal(loc=15, scale=10, size=len(date_range))
+    df['temperature'] += np.linspace(-5, 5, len(date_range))
+    df.loc[outliers, 'temperature'] *= -1
+    
+    df['humidity'] = np.random.uniform(low=20, high=100, size=len(date_range))
+    df['humidity'] += np.sin(np.linspace(0, 2 * np.pi, len(date_range))) * 10
+    
+    df['rainfall'] = np.random.binomial(n=1, p=0.3, size=len(date_range))
+    
+    df['holiday'] = np.random.binomial(n=1, p=0.1, size=len(date_range))
+    
+    df['day_of_week'] = df['date'].dt.day_name()
+    
+    texts = ['A', 'B', 'C', 'D', 'E']
+    df['text_data'] = np.random.choice(texts, size=len(date_range))
+    
+    weathers = ['Hot', 'Cold', 'Sunny', 'Rain', 'Cloud']
+    df['weather'] = np.random.choice(weathers, size=len(date_range))
+    
+    return df
