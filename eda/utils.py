3d23fe364a56de0cf5ddcaaa4237f324716f0c05
Linfo-KR <linfo4931@gmail.com>
2024-06-27 10:36:43 +0900

    Move calculate_ndim() to graph_generator

--- eda/utils.py
+++ eda/utils.py
@@ -1,26 +1,6 @@
 import numpy as np
 import pandas as pd
 
-def calculate_ndim(col_length=int):
-    """
-    col_length에 맞는 subplot의 차원을 계산하는 함수
-
-    Parameters:
-    -----------
-    col_length : int
-        subplot에 전달할 열의 길이
-        
-    Returns:
-    --------
-    n : int
-        subplot의 차원
-    """
-    if not isinstance(col_length, int):
-        raise ValueError(f"col_length or hue_length should be an int, got {type(col_length)} instead.")
-    
-    n = int(np.ceil(np.sqrt(col_length)))
-    
-    return n
 
 def generate_testset():
     np.random.seed(42)
