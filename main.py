b75130e83ca677da17acf9c6e4f602fab2ed06d2
Linfo-KR <linfo4931@gmail.com>
2024-06-27 10:37:52 +0900

    test code

--- main.py
+++ main.py
@@ -1,57 +1,44 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
-from modules.eda import EDA
-
+from eda.data_loader import DataLoader
+from eda.data_summary import DataSummary
+from eda.visualization import Visualization
+from eda.report_generator import PDF, ReportGenerator
+from eda.utils import *
 
 def main():
-    dict_data = {'Name': ['Alice', 'Bob', 'Charlie'],
-             'Age': [25, 30, 35],
-             'Gender': ['F', 'M', 'M']}
-
-    list_data = [['Alice', 25, 'F'],
-                ['Bob', 30, 'M'],
-                ['Charlie', 35, 'M']]
-
-    np_data = np.array([['Alice', 25, 'F'],
-                        ['Bob', 30, 'M'],
-                        ['Charlie', 35, 'M']])
-
-    df_data = pd.DataFrame({'Name': ['Alice', 'Bob', 'Charlie'],
-                            'Age': [25, 30, 35],
-                            'Gender': ['F', 'M', 'M']})
+    vessel = pd.read_csv('./test/test_data/vessel_data_04.csv', encoding='cp949')
+    vessel = vessel.drop('VVD', axis=1)
+    vessel = vessel.drop('CALL_SIGN', axis=1)
+    vessel = vessel.drop('ROUTE', axis=1)
+    for i in range(4):
+        vessel = vessel.replace({'BTH_NO': i}, f'T{i}')
+    vessel['ATB'] = pd.to_datetime(vessel['ATB'])
+    vessel['ATD'] = pd.to_datetime(vessel['ATD'])
+    vessel['WRK_START_DTE'] = pd.to_datetime(vessel['WRK_START_DTE'])
+    vessel['WRK_END_DTE'] = pd.to_datetime(vessel['WRK_END_DTE'])
+    vessel['ATA'] = pd.to_datetime(vessel['ATA'])
+    vessel['ATG'] = pd.to_datetime(vessel['ATG'])
+    vessel['ETB'] = pd.to_datetime(vessel['ETB'])
+    vessel['ETD'] = pd.to_datetime(vessel['ETD'])
+    texts = ['A', 'B', 'C', 'D', 'E']
+    vessel['grade'] = np.random.choice(texts, size=len(vessel))
+    regions = ['NAEC', 'NAWC', 'SAEC', 'SAWC', 'ASIA', 'EUR']
+    vessel['region'] = np.random.choice(regions, size=len(vessel))
     
-    test_data = pd.read_csv('./test/test_data/titanic.csv')
-    vessel_data = pd.read_csv('./test/test_data/vessel_data_03.csv', encoding='cp949')
     
-    test00 = EDA(test_data)
-    test01 = EDA(dict_data)
-    test02 = EDA(list_data)
-    test03 = EDA(np_data)
-    test04 = EDA(df_data)
-    test05 = EDA(vessel_data)
+    rp01 = ReportGenerator(vessel, './test/report_test/vesseldata', 'PNIT BerthPlan')
+    viz01 = Visualization(vessel, './test/visualize_test/vesseldata/')
+    rp01.generate_report()
+    viz01.visualize()
     
-    test00.save_markdown('./test/eda_test/md_test00.md')
-    test01.save_markdown('./test/eda_test/md_test01.md')
-    test02.save_markdown('./test/eda_test/md_test02.md')
-    test03.save_markdown('./test/eda_test/md_test03.md')
-    test04.save_markdown('./test/eda_test/md_test04.md')
-    test05.save_markdown('./test/eda_test/md_test05.md')
-    
-    test00.save_pdf('./test/eda_test/pdf_test00.pdf')
-    test01.save_pdf('./test/eda_test/pdf_test01.pdf')
-    test02.save_pdf('./test/eda_test/pdf_test02.pdf')
-    test03.save_pdf('./test/eda_test/pdf_test03.pdf')
-    test04.save_pdf('./test/eda_test/pdf_test04.pdf')
-    test05.save_pdf('./test/eda_test/pdf_test05.pdf')
-    
-    test00.visualize('./test/eda_test')
-    test01.visualize('./test/eda_test/')
-    test02.visualize('./test/eda_test')
-    test03.visualize('./test/eda_test/')
-    test04.visualize('./test/eda_test')
-    test05.visualize('./test/eda_test/')
-
+    # weather = generate_testset()
+    # rp02 = ReportGenerator(weather, './test/report_test/weatherdata', 'Weather Data')
+    # viz02 = Visualization(weather, './test/visualize_test/weatherdata')
+    # rp02.generate_report()
+    # viz02.visualize()
 
 if __name__ == '__main__':
     main()
