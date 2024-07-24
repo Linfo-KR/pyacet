9b725c9f94fb53004f04780d1317a50052aa2bb6
Linfo-KR <linfo4931@gmail.com>
2024-07-25 00:27:08 +0900

    Update library setup

--- setup.py
+++ setup.py
@@ -6,12 +6,14 @@ with open('README.md', encoding='utf-8') as f:
 setup(
     name = 'pyacet',
     version = '0.1.0',
-    packages = find_packages(),
+    packages = ['pyacet', 'pyacet.modules'],
+    package_data = {'pyacet': ['modules/*.py']},
+    include_package_data = True,
     description = 'Python Automated Custom EDA Toolkit',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     author = 'YeongIL Kim',
     author_email = 'linfo4931@gmail.com',
-    url = 'https://github.com/Linfo-KR/Library',
+    url = 'https://github.com/Linfo-KR/pyacet',
     install_requires = open('requirements.txt').read().splitlines(),
 )
