75f441f073ce07fafc5d9ef89dee61c1dc409276
Linfo-KR <linfo4931@gmail.com>
2024-07-25 00:03:19 +0900

    Revise README encoding(utf-8)

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name = 'pyacet',
     version = '0.1.0',
     packages = find_packages(),
     description = 'Python Automated Custom EDA Toolkit',
-    # long_description = open('README.md').read(),
-    # long_description_content_type = 'text/markdown',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
     author = 'YeongIL Kim',
     author_email = 'linfo4931@gmail.com',
     url = 'https://github.com/Linfo-KR/Library',
