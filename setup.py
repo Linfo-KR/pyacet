27bf041264f863b52e8a643b75e2a7b364cfbd14
Linfo-KR <linfo4931@gmail.com>
2024-07-24 16:56:56 +0900

    Revise encoding error

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name = 'pyacet',
     version = '0.1.0',
     packages = find_packages(),
     description = 'Python Automated Custom EDA Toolkit',
-    long_description = open('README.md').read(),
+    long_description = long_description,
     long_description_content_type = 'text/markdown',
     author = 'YeongIL Kim',
     author_email = 'linfo4931@gmail.com',
